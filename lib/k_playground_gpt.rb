53fcd71bb3a5e443a99764855766b2c3ae1ba2bf
David Cruwys <david@ideasmen.com.au>
2023-08-13 15:01:48 +1000

    first commit

---
+++ lib/k_playground_gpt.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "k_playground_gpt/version"
+
+module KPlaygroundGpt
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/k_playground_gpt/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module KPlaygroundGpt
+  VERSION = "0.1.0"
+end
