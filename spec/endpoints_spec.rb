5eaed6a4d977921008ffaa7c2252266e6a0c7de6
David Cruwys <david@ideasmen.com.au>
2023-08-17 16:06:00 +1000

    List models

--- spec/endpoints_spec.rb
+++ spec/endpoints_spec.rb
@@ -1,1 +1,25 @@
 # frozen_string_literal: true
+
+RSpec.describe 'Endpoints', :openai do
+  OpenAI.configure do |config|
+    config.access_token = ENV.fetch('OPENAI_ACCESS_TOKEN')
+  end
+
+  let(:client) { OpenAI::Client.new }
+
+  context 'when using models' do
+    describe '#list' do
+      it 'returns a list of models (JSON)' do
+        response = client.models.list
+
+        puts JSON.pretty_generate(response)
+      end
+
+      it 'returns a list of models with just id and type' do
+        response = client.models.list['data'].map { |entry| { id: entry['id'], type: entry['object'] } }
+
+        tp response.sort_by { |entry| entry[:id] }
+      end
+    end
+  end
+end
