1a81f2d8bc21e24b868a4f0a80c57cc56da3653f
David Cruwys <david@ideasmen.com.au>
2023-08-18 07:50:05 +1000

    Edits

--- spec/endpoints_spec.rb
+++ spec/endpoints_spec.rb
@@ -54,6 +54,71 @@ RSpec.describe 'Endpoints', :openai do
           L.kv 'Prompt tokens', response['usage']['prompt_tokens']
           L.kv 'Completion tokens', response['usage']['completion_tokens']
           L.kv 'Total tokens', response['usage']['total_tokens']
+
+          L.warn 'Completions is a legacy endpoint and will be removed in the future. Use the "Chat" endpoint instead.'
+        end
+      end
+    end
+  end
+  # it 'edit text based on an instruction' do
+  #   edited_text_response = client.edits(
+  #     parameters: {
+  #       model: 'text-davinci-edit-001',
+  #       input:
+  #       'What day of the wek is it?',
+  #       instruction: 'Fix the spelling mistakes'
+  #     }
+  #   )
+  #   edited_text_response = edited_text_response.to_h
+
+  #   puts JSON.pretty_generate(edited_text_response)
+
+  #   expect(edited_text_response).to be_a(Hash)
+  # end
+  describe '.edits' do
+    let(:model) { 'text-davinci-edit-001' }
+    let(:parameters) { { model: model, input: prompt, instruction: instruction } }
+
+    context 'when fixing spelling mistakes' do
+      let(:prompt) { 'What day of the wek is it?' }
+      let(:instruction) { 'Fix the spelling mistakes' }
+
+      it 'returns edited response' do
+        response = client.edits(parameters: parameters)
+
+        L.kv 'Prompt', prompt
+        L.kv 'Instruction', instruction
+        L.kv 'Edited text', response['choices'][0]['text']
+
+        L.section_heading 'Usage'
+
+        L.kv 'Prompt tokens', response['usage']['prompt_tokens']
+        L.kv 'Completion tokens', response['usage']['completion_tokens']
+        L.kv 'Total tokens', response['usage']['total_tokens']
+
+        L.warn 'Edits is a legacy endpoint and will be removed in the future. Use the "Chat" endpoint instead.'
+      end
+    end
+
+    context 'when fixing misquotes' do
+      let(:prompt) { 'The quick brown fox jumped over the lazy cat' }
+      let(:instruction) { 'Fix misquotations' }
+
+      5.times do
+        it 'returns edited response' do
+          response = client.edits(parameters: parameters)
+
+          L.kv 'Prompt', prompt
+          L.kv 'Instruction', instruction
+          L.kv 'Edited text', response['choices'][0]['text']
+
+          L.section_heading 'Usage'
+
+          L.kv 'Prompt tokens', response['usage']['prompt_tokens']
+          L.kv 'Completion tokens', response['usage']['completion_tokens']
+          L.kv 'Total tokens', response['usage']['total_tokens']
+
+          L.warn 'Edits is a legacy endpoint and will be removed in the future. Use the "Chat" endpoint instead.'
         end
       end
     end
